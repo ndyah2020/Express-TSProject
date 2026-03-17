c80e4a73e1e6e732b59d0015e97f9d2e1413d6f6
hoaibao3112 <hoaibao3112@example.com>
2026-03-17 18:45:53 +0700

    fix: format phone property and ensure IcustomerDocument extends Icustomer correctly

--- src/interfaces/customers.interface.ts
+++ src/interfaces/customers.interface.ts
@@ -1,8 +1,18 @@
 export interface Icustomer {
     name: string,
     email: string,
-    phone:string,
+    phone: string,
     address: string,
     createdAt: Date,
     updatedAt: Date,
+}
+export interface IcustomerDocument extends Icustomer, Document {
+    _id: string,
+    name: string,
+    email: string,
+    phone: string,
+    address: string,
+    createdAt: Date,
+    updatedAt: Date,
+
 }
